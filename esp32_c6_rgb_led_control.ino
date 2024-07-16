f2181dec5c1ff64c52e7971b5707f31fb8252d33
Extravagant <40579109+h4fide@users.noreply.github.com>
2024-07-16 03:53:28 +0100

    Update esp32_c6_rgb_led_control.ino

--- esp32_c6_rgb_led_control.ino
+++ esp32_c6_rgb_led_control.ino
@@ -1,39 +1,49 @@
 #include <Adafruit_NeoPixel.h>
 
-#define LED_PIN 8  // The ESP32-C6 pin connected to the built-in RGB LED
-#define NUM_LEDS 1
+constexpr uint8_t LED_PIN = 8;
+constexpr uint8_t NUM_LEDS = 1;
 
 Adafruit_NeoPixel rgbLed(NUM_LEDS, LED_PIN, NEO_GRB + NEO_KHZ800);
 
-
 struct RGB {
-    uint8_t red;
-    uint8_t green;
-    uint8_t blue;
+    uint8_t r, g, b;
 };
 
-const RGB COLOR_RED = {255, 0, 0};
-const RGB COLOR_GREEN = {0, 255, 0};
-const RGB COLOR_BLUE = {0, 0, 255};
-const RGB COLOR_WHITE = {255, 255, 255};
-// .....Feel free to add more colors as needed0
-const RGB LED_OFF = {0, 0, 0};
+constexpr RGB COLOR_OFF   = {0, 0, 0};
+
+constexpr RGB COLOR_RED   = {255, 0, 0};
+constexpr RGB COLOR_GREEN = {0, 255, 0};
+constexpr RGB COLOR_BLUE  = {0, 0, 255};
+constexpr RGB COLOR_WHITE = {255, 255, 255};
+// ...Feel free to add more colors here...
+constexpr RGB CUSTOM_COLOR = {255, 0, 255}; 
+
+
 
 void setup() {
-    rgbLed.begin();  
-    rgbLed.show();  
+    rgbLed.begin(); 
+    rgbLed.show(); 
 }
 
 void setColor(const RGB& color) {
-    rgbLed.setPixelColor(0, color.red, color.green, color.blue);
+    rgbLed.setPixelColor(0, rgbLed.Color(color.r, color.g, color.b));
     rgbLed.show();
 }
 
-void loop() {
+void blinkColor(const RGB& color, unsigned long duration) {
+    setColor(color);
+    delay(duration);
+    setColor(COLOR_OFF);
+    delay(duration);
+}
 
-    // Example: Blink the built-in LED
-    setColor(COLOR_RED);
-    delay(1000);
-    setColor(LED_OFF);
-    delay(1000);
+
+void loop() {
+    constexpr unsigned long BLINK_DURATION = 500;
+
+    blinkColor(COLOR_RED, BLINK_DURATION);
+    blinkColor(COLOR_GREEN, BLINK_DURATION);
+    blinkColor(COLOR_BLUE, BLINK_DURATION);
+    blinkColor(COLOR_WHITE, BLINK_DURATION);
+    blinkColor(CUSTOM_COLOR, BLINK_DURATION);
 }
